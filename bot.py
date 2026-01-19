5cbc218f2b545986075ff2f840b0afeff77e190d
meflover <guextywq@gmail.com>
2026-01-19 15:46:25 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,7 +1,10 @@
 from aiogram import Bot
 from config import key
 from aiogram import Dispatcher
+from aiogram import Router
 
+rt = Router()
 dp = Dispatcher()
+dp.include_router(rt)
 bot = Bot(token=key)
 print('SANDBOX BOT')
