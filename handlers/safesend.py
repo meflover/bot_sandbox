ea92f3e0eb372874c6d2d3899ddc2bb617ebb6e9
meflover <guextywq@gmail.com>
2026-01-22 22:13:47 +0300

    Add files via upload

--- handlers/safesend.py
+++ handlers/safesend.py
@@ -29,31 +29,36 @@ async def safe_send(send_func, *args, **kwargs):
 
 
 async def answer(id_, text):
-    await safe_send(bot.send_message,id_,text)
+    msg = await safe_send(bot.send_message,id_,text,protect_content=True)
+    return msg.message_id
 async def reply(message, text):
     await safe_send(message.reply, text)
-
+    return msg.message_id
 
 async def dropvoice(user_id, file_id):
-    await safe_send(bot.send_voice, user_id, file_id)
-
+    msg = await safe_send(bot.send_voice, user_id, file_id)
+    return msg.message_id
 async def drop_video_note(user_id, file_id):
-    await safe_send(bot.send_video_note, user_id, file_id)
-
+    msg = await safe_send(bot.send_video_note, user_id, file_id)
+    return msg.message_id
 async def dropaudio(user_id, file_id):
-    await safe_send(bot.send_audio, user_id, file_id)
+    msg = await safe_send(bot.send_audio, user_id, file_id)
+    return msg.message_id
 async def dropsticker(user_id, file_id):
-    await safe_send(bot.send_sticker, user_id, file_id)
+    msg = await safe_send(bot.send_sticker, user_id, file_id)
+    return msg.message_id
 async def dropgif(user_id, file_id):
-    await safe_send(bot.send_animation, user_id, file_id)
-
+    msg = await safe_send(bot.send_animation, user_id, file_id)
+    return msg.message_id
 async def dropmedia(user_id, media):
-    await safe_send(bot.send_media_group, user_id, media)
+    msg = await safe_send(bot.send_media_group, user_id, media)
+    return msg.message_id
 async def dropphoto_html(user_id, photo_id, text =''):
-    await safe_send(bot.send_photo, user_id, photo_id, caption=text, parse_mode='HTML')
-
+    msg = await safe_send(bot.send_photo, user_id, photo_id, caption=text, parse_mode='HTML')
+    return msg.message_id
 async def dropphoto(user_id, photo_id, text=''):
-    await safe_send(bot.send_photo, user_id, photo_id, caption=text)
+    msg = await safe_send(bot.send_photo, user_id, photo_id, caption=text)
+    return msg.message_id
 async def delete_message(chat_id, mess_id):
     try:
         await safe_send(bot.delete_message, chat_id, mess_id)
@@ -72,11 +77,11 @@ async def dropkeyboard(user_id: int, one_time, buttons: list[str], text: str = "
         one_time_keyboard = one_time
     )
 
-    await safe_send(bot.send_message,
+    msg = await safe_send(bot.send_message,
         chat_id=user_id,
         text=text,
         reply_markup=kb)
-
+    return msg.message_id
 async def dropinlinekeyboard(
     user_id: int,
     buttons: list[str],
@@ -95,9 +100,37 @@ async def dropinlinekeyboard(
 
     kb = InlineKeyboardMarkup(inline_keyboard=keyboard)
 
-    await safe_send(bot.send_message,
+    msg = await safe_send(bot.send_message,
         chat_id=user_id,
         text=text,
         reply_markup=kb)
-    
+    return msg.message_id
 
+async def edit_text(chat_id: int, message_id: int, text: str):
+    await safe_send(
+        bot.edit_message_text,
+        chat_id=chat_id,
+        message_id=message_id,
+        text=text
+    )
+
+async def edit_inlinekeyboard(chat_id, message_id, buttons):
+    if buttons:
+        keyboard = [
+                [
+                    InlineKeyboardButton(
+                        text=b,
+                        callback_data=f"btn:{b}"
+                    )
+                    for b in buttons[i:i + 4]
+                ]
+                for i in range(0, len(buttons), 4)
+            ]
+
+        kb = InlineKeyboardMarkup(inline_keyboard=keyboard)
+    else:
+        kb = None
+    await bot.edit_message_reply_markup(
+        chat_id=chat_id,
+        message_id=message_id,
+        reply_markup=kb)
